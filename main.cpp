05506e06052e561ba3d1e1821851a2472538e130
Junbo ZHAO <zhaojunbo1992chasing@gmail.com>
2014-08-19 14:32:54 -0400

    strech texts and figlet added in

--- main.cpp
+++ main.cpp
@@ -1,47 +1,53 @@
+// TODO FIGlet
+
 #include <iostream>
 #include <string>
-#include <vector>
+#include <time.h>
+#include <cstdlib>
+#include <sys/ioctl.h>
 
 using namespace std;
 
-class NYCFonts{
- public:
-   NYCFonts(int, int, int);
-   ~NYCFonts();
-   void MoveLeft();
-   int GetPoint();
- private:
-   int size;
-   int point_x; // coordinates of the left-up point
-   int point_y;
-};
+//int g_length = 1000;
+// Get the console width.
+struct winsize w;
+ioctl(0, TIOCGWINSZ, &w);
+int g_length = w.ws_col;
 
-NYCFonts::NYCFonts(int _size, int _point_x, int _point_y) :
-         size(_size),
-         point_x(_point_x),
-         point_y(_point_y) {};
-
-NYCFonts::~NYCFonts() {}
-
-void NYCFonts::MoveLeft(){
-  --point_x ;
+void wait(long m){
+  long timeout = clock() + m;
+  while(clock() < timeout){
+    continue;
+  }
 }
 
-int NYCFonts::GetPoint(){
-  return point_x;
+string strech(string msg, int Llen = 0, int Rlen = g_length){
+  string _msg, Ltemp, Rtemp;
+  for (int i = 0; i < Llen; i++) {
+    Ltemp += "\\ ";
+  }
+  //for (int i = 0; i < Rlen; i++) {
+  //  Rtemp += "\\ ";
+  //}
+  //Ltemp.assign(Llen, "\ ");
+  Rtemp.assign(Rlen, ' ');
+  _msg = Ltemp + msg + Rtemp;
+  return _msg;
 }
 
 
 int main(int argc, const char *argv[])
 {
-  NYCFonts nycfonts(0, 200, 0);
-  int i;
-  while(nycfonts.GetPoint() != 0){
-    for (i = 0; i < nycfonts.GetPoint(); i++) {
-      std::cout << " " ;
-    }
-    std::cout << "Hello NYC!" << std::endl;
-    nycfonts.MoveLeft();
+  std::cout << g_length << std::endl;
+  string nyc = "Hello NYC!";
+  string font_tool = "figlet ";
+  int x = 0;
+  while(x < g_length){
+    //std::cout << strech(nyc, x, g_length-x) << std::endl;
+    system((font_tool + strech(nyc, x, g_length-x)).c_str());
+    wait(50000);
+    system("clear");
+    x ++;
   }
   return 0;
 }
